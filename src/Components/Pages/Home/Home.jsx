0f39d389b815b43687e2cccf70195a62a7560c86
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-15 22:23:25 -0600

    file path changes

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Top from './sections/top'
-import Middle from './sections/middle'
+import Middle from './sections/Center'
 import Bottom from './sections/bottom'
+
 
 export default function Home () {
   return (
