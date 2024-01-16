15872c640100d13bd468e483f85db968cc7565d7
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-15 22:26:44 -0600

    files renamed

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Top from './sections/top'
 import Middle from './sections/Center'
-import Bottom from './sections/bottom'
+import Bottom from './sections/Lower'
+
 
 
 
