10fcfce69d23f20b7da890870ea775c65fcc7978
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-19 15:34:20 -0600

    about text inserted

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -16,13 +16,8 @@ export const About = () => {
         <img className={imageStyle} src={aboutPhoto} alt="photo of thomas quinn" />
         <div className={divStyle}>
           <h1 className={titleStyle}>FROM ONE BEARD TO ANOTHER</h1>
-          <p className={textStyle}>Lorem ipsum dolor sit amet consectetur, adipisicing 
-          elit. Aliquam suscipit dolorem sequi rem quis at iste 
-          illum, consectetur enim cupiditate non! Fuga eius aliquam
-          ea accusamus nulla dolorem laborum maxime dignissimos, 
-          fugit id quibusdam atque! Voluptas quos distinctio
-            asperiores eaque optio voluptatibus nostrum nemo. 
-            Animi impedit quis reprehenderit? Dicta, libero.</p>
+          <p className={textStyle}>I am Thomas Quinn, hailing from Dover, New Hampshire. Throughout my journey in the realm of bearded life, the aspiration to create my own line of beard oil has been a persistent dream. Drawing inspiration from scents and blends found in beard oils worldwide, I dedicated years to formulating a product that promotes the health of beards. Amid the busyness of life and the growth of my family, comprising my supportive wife and two boys, my dream has materialized into a reality.
+</p>
         </div>
       </div>
     </article>
