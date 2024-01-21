b4151c95c03d7b1d6cca77cda3a72dc93c1e1e40
Matthew Guptill <matthewdguptill7@gmail.com>
2024-01-20 20:13:49 -0600

    shopify added for forest blend oil

---
+++ src/Components/Pages/Home/sections/buy-button-forest.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+export default function ShopifyBuyButtonForest () {
+    return (
+        <>
+        <div id='product-component-1705793595499'></div>
+
+
+        {function () {
+        let scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+        if (window.ShopifyBuy) {
+            if (window.ShopifyBuy.UI) {
+            ShopifyBuyInit();
+            } else {
+            loadScript();
+            }
+        } else {
+            loadScript();
+        }
+        function loadScript() {
+            let script = document.createElement('script');
+            script.async = true;
+            script.src = scriptURL;
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+            script.onload = ShopifyBuyInit;
+        }
+        function ShopifyBuyInit() {
+            let client = ShopifyBuy.buildClient({
+            domain: '3e988b-3.myshopify.com',
+            storefrontAccessToken: 'f83b19b39b8275d4f4d6df297d4f1fa1',
+            });
+            ShopifyBuy.UI.onReady(client).then(function (ui) {
+            ui.createComponent('product', {
+                id: '8215419945185',
+                node: document.getElementById('product-component-1705793595499'),
+                moneyFormat: '%24%7B%7Bamount%7D%7D',
+                options: {
+        "product": {
+            "styles": {
+            "product": {
+                "@media (min-width: 601px)": {
+                "max-width": "calc(25% - 20px)",
+                "margin-left": "20px",
+                "margin-bottom": "50px"
+                }
+            },
+            "button": {
+                "padding-left": "100px",
+                "padding-right": "100px"
+            }
+            },
+            "text": {
+            "button": "Add to cart"
+            }
+        },
+        "productSet": {
+            "styles": {
+            "products": {
+                "@media (min-width: 601px)": {
+                "margin-left": "-20px"
+                }
+            }
+            }
+        },
+        "modalProduct": {
+            "contents": {
+            "img": false,
+            "imgWithCarousel": true,
+            "button": false,
+            "buttonWithQuantity": true
+            },
+            "styles": {
+            "product": {
+                "@media (min-width: 601px)": {
+                "max-width": "100%",
+                "margin-left": "0px",
+                "margin-bottom": "0px"
+                }
+            },
+            "button": {
+                "padding-left": "100px",
+                "padding-right": "100px"
+            }
+            },
+            "text": {
+            "button": "Add to cart"
+            }
+        },
+        "option": {},
+        "cart": {
+            "text": {
+            "total": "Subtotal",
+            "button": "Checkout"
+            }
+        },
+        "toggle": {}
+        },
+            });
+            });
+        }
+        }()
+        }
+        </>
+    )
+}
--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import beachDay from '/src/assets/photos/beach-day-white.jpg'
 import forestBlend from '/src/assets/photos/forest-blend-white.jpg'
 import ShopifyBuyButtonBeach from './Home/sections/buy-button'
+import ShopifyBuyButtonForest from './Home/sections/buy-button-forest'
+
 
 export const Shop = () => {
 
   const article = ''
   const beach = 'bg-[url("../images/beach.jpg")] h-fit lg:h-dvh bg-cover flex flex-col justify-center items-center pt-32 pb-10'
   const forest = 'bg-[url("../images/forest-products.jpg")] h-fit lg:h-dvh bg-cover flex flex-col justify-center items-center pt-10 pb-10'
-  const oilCon = 'w-11/12 h-fit bg-black rounded-md mx-4 mt-4 py-7 flex flex-col items-center justify-evenly lg:flex-row lg:w-4/5 lg:mx-0 lg:px-10 lg:h-2/3'
+  const oilCon = 'w-11/12 h-fit  rounded-md mx-4 mt-4 py-7 flex flex-col items-center justify-evenly lg:flex-row lg:w-4/5 lg:mx-0 lg:px-10 lg:h-2/3'
   // const images = 'w-2/3 rounded-md lg:h-72 lg:w-auto'
   const title = 'font-titles text-black text-3xl underline lg:place-self-start lg:pl-24'
-  const textCon = 'flex flex-col items-center lg:w-[50%]'
-  const text = 'text-white px-5 text-center py-7  '
-  const link = 'text-2xl px-8 py-2 bg-white rounded-md hover:bg-green-900 hover:text-white trasition-all duration-200'
+  const textCon = 'flex flex-col items-center lg:w-[50%] bg-black/50'
+  const text = 'lg:text-3xl text-white font-semibold px-5 text-center py-7  '
 
  
 
@@ -24,16 +25,17 @@ export const Shop = () => {
         <div className={oilCon}>
           <ShopifyBuyButtonBeach />
           <div className={textCon}>
-            <p className={text}>Dive into the ultimate beachside bliss with our &quot;Beach Day&quot; beard oil blend!
-                          Envelop yourself in the soothing caress of sandalwood, reminiscent of warm,
-                          sun-kissed shores. Lavender adds a touch of relaxation as if you&#39;re lounging on a
-                          hammock under swaying palm trees. The zesty notes of lemon transport you to a
-                          tropical paradise, where each strand of your beard is kissed by the ocean
-                          breeze. It&#39;s like a vacation for your facial follicles! Embrace the laid-back vibes,
-                          and let your beard be the symbol of your endless summer. &quot;Beach Day&quot; blend –
-                          because who says your beard can&#39;t have its own day in the sun?
+            <p className={text}>
+              Dive into the ultimate beachside bliss with our &quot;Beach Day&quot; beard oil blend!
+              Envelop yourself in the soothing caress of sandalwood, reminiscent of warm,
+              sun-kissed shores. Lavender adds a touch of relaxation as if you&#39;re lounging on a
+              hammock under swaying palm trees. The zesty notes of lemon transport you to a
+              tropical paradise, where each strand of your beard is kissed by the ocean
+              breeze. It&#39;s like a vacation for your facial follicles! Embrace the laid-back vibes,
+              and let your beard be the symbol of your endless summer. &quot;Beach Day&quot; blend –
+              because who says your beard can&#39;t have its own day in the sun?
             </p>
-            <a className={link} href="/">Buy</a>
+            
           </div>
         </div>
 
@@ -43,7 +45,7 @@ export const Shop = () => {
       <section className={forest}>
         <h1 className={title}>FOREST BLEND</h1>
         <div className={oilCon}>
-         
+         <ShopifyBuyButtonForest />
           <div className={textCon}>
             <p className={text}>Introducing our &quot;Forest Blend&quot; beard oil, the secret potion to transform your facial
                                 fuzz into a wilderness wonder! Crafted with the essence of majestic cedarwood
@@ -55,7 +57,7 @@ export const Shop = () => {
                                 our Forest Blend – because your beard deserves a touch of nature&#39;s mischievous
                                 charm!
             </p>
-            <a className={link} href="/">Buy</a>
+           
           </div>
         </div>
       </section>
